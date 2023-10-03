535e728aa00c4f89b0dc4d7dc372f37932fce2df
student_2333 <lgc2333@126.com>
2023-10-04 05:28:26 +0800

    chore

--- nonebot_plugin_multincm/const.py
+++ nonebot_plugin_multincm/const.py
@@ -1,12 +1,10 @@
 from pathlib import Path
 
+MUSIC_LINK_TEMPLATE = "https://music.163.com/{type}?id={id}"
+
+
 DATA_PATH = Path().cwd() / "data" / "multincm"
 for _p in (DATA_PATH,):
     _p.mkdir(parents=True, exist_ok=True)
 
-FILE_CACHE_PATH = DATA_PATH / "file_cache.json"
-
 RES_DIR = Path(__file__).parent / "res"
-
-
-MUSIC_LINK_TEMPLATE = "https://music.163.com/{type}?id={id}"
