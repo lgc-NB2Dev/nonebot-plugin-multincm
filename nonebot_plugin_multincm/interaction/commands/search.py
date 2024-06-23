7031608152a36960203857b1f4d058615a578f49
student_2333 <lgc2333@126.com>
2024-06-23 16:40:35 +0800

    up

--- nonebot_plugin_multincm/interaction/commands/search.py
+++ nonebot_plugin_multincm/interaction/commands/search.py
@@ -1,9 +1,9 @@
 import asyncio
-from typing import Any, Optional, Tuple, Type, cast
+from typing import Optional, Tuple, Type, cast
 
 from cookit.loguru.common import logged_suppress
 from cookit.nonebot.alconna import RecallContext
-from nonebot import logger, on_command, on_regex
+from nonebot import logger, on_command
 from nonebot.adapters import Message as BaseMessage
 from nonebot.matcher import Matcher, current_matcher
 from nonebot.params import ArgPlainText, CommandArg
@@ -12,11 +12,9 @@ from nonebot_plugin_alconna.uniseg import UniMessage
 from nonebot_plugin_waiter import prompt
 
 from ...config import config
-from ...const import SHORT_URL_REGEX, URL_REGEX
 from ...data_source import (
     BaseSearcher,
     BaseSong,
-    BaseSongList,
     GeneralGetPageReturn,
     GeneralSearcher,
     GeneralSongList,
@@ -26,9 +24,7 @@ from ...data_source import (
     registered_searcher,
 )
 from ...render import render_list_resp
-from ..cache import set_cache
 from ..message import construct_info_msg, send_song
-from ..resolver import IsAutoResolve, ResolvedItem
 
 KEY_SEARCHER = "searcher"
 KEY_KEYWORD = "keyword"
@@ -196,28 +192,4 @@ def __register_searcher_matchers():
         do_reg(k, v)
 
 
-async def resolve_handler(
-    matcher: Matcher,
-    result: ResolvedItem,
-    is_auto_resolve: IsAutoResolve,
-):
-    result_it: Any = cast(Any, result)  # fuck that annoying weak type annotation
-    if is_auto_resolve and isinstance(result, BaseSongList):
-        await (await construct_info_msg(result_it, tip_command=True)).send()
-    else:
-        await handle_song_or_list(result_it, matcher, send_init_info=True)
-    await set_cache(result_it)
-
-
-def __register_resolve_matchers():
-    matcher = on_command("解析", aliases={"resolve", "parse", "get"})
-    matcher.handle()(resolve_handler)
-    if config.ncm_auto_resolve:
-        reg_matcher = on_regex(URL_REGEX)
-        reg_matcher.handle()(resolve_handler)
-        reg_short_matcher = on_regex(SHORT_URL_REGEX)
-        reg_short_matcher.handle()(resolve_handler)
-
-
 __register_searcher_matchers()
-__register_resolve_matchers()
